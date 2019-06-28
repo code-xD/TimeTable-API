1dd8257a978b4f8ff8b53567d3b0c61e02380e42
Shivansh Anand <shivansh586@gmail.com>
2019-06-29 00:37:37 +0530

    Foodcourts fixed.

--- timetable/models.py
+++ timetable/models.py
@@ -17,7 +17,7 @@ class FoodCourt(models.Model):
     FID = models.CharField(max_length=200, unique=True, primary_key=True)
 
     def __str__(self):
-        return self.FID
+        return self.name
 
     def save(self, *args, **kwargs):
         self.FID = self.name+'-'+str(self.user)
